7645f07c7574e230869c1ccb347969f0d0d73f20
cberleux <clement.berleux@gmail.com>
2024-11-26 15:13:50 +0100

    Ajout page d'accueil

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}

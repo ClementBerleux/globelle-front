00be580253a03daa1e1cb470e1bbdf4accf91630
cberleux <clement.berleux@gmail.com>
2024-11-26 16:09:42 +0100

    Ajout curseur

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      cursor: {
+        vernis: 'url("/images/vernis.png"), pointer',
+      },
+    },
   },
   plugins: [],
 };

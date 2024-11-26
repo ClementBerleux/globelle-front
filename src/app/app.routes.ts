d0f5797a2d6e89a8101174ab094a28c1ef502800
cberleux <clement.berleux@gmail.com>
2024-11-26 15:49:55 +0100

    Ajout page login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: AccueilComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: AccueilComponent },
 ];

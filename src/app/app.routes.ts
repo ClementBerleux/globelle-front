b1d412f8ee2af9e315b662cca73a268f932ab44a
cberleux <clement.berleux@gmail.com>
2024-11-27 12:12:27 +0100

    Ajout page results

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { LoginComponent } from './components/login/login.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 export const routes: Routes = [
   { path: '', component: AccueilComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'result', component: SearchResultsComponent },
   { path: '**', component: AccueilComponent },
 ];
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './search-results.component.html',
-  styleUrl: './search-results.component.css'
+  styleUrl: './search-results.component.css',
 })
 export class SearchResultsComponent {
+  public constructor(public route: ActivatedRoute) {}
 
+  public search: string = '';
+
+  ngOnInit() {
+    this.route.queryParamMap.subscribe((params) => {
+      this.search = params.get('search') || '';
+    });
+  }
 }

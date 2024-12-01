a2cc5700cc4d1fc42f860ec5fb0472b0f900536e
cberleux <clement.berleux@gmail.com>
2024-12-01 22:33:39 +0100

    Ajout reactive form

--- src/app/components/booking-page/booking-page.component.ts
+++ src/app/components/booking-page/booking-page.component.ts
@@ -4,11 +4,12 @@ import { ProviderService } from '../../services/provider.service';
 import { Service } from '../../models/service';
 import { Provider } from '../../models/provider';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './booking-page.component.html',
   styleUrl: './booking-page.component.css',
 })
@@ -16,8 +17,12 @@ export class BookingPageComponent {
   readonly services = signal<Service[]>([]);
   readonly provider = signal<Provider>(new Provider);
   readonly idProvider = signal(0)
+  readonly selection = signal('')
   public today: Date = new Date(Date.now())
   public todayString: string;
+  public formGroup = new FormGroup({
+    service: new FormControl('', { nonNullable: true })
+  })
 
   constructor(private route: ActivatedRoute, public location: Location, public providerService: ProviderService) {
     this.todayString = this.today.getFullYear() + '-' + this.today.getMonth() + '-' + this.today.getDate()
@@ -28,5 +33,13 @@ export class BookingPageComponent {
     this.providerService.getPrestataire(this.idProvider()).subscribe((data) => this.provider.set(data))
     this.providerService.getServices(this.idProvider()).subscribe((data) => this.services.set(data));
     // console.log('Date : ' + Date.now())
+  }
+
+  onSubmit(): void {
+    // console.log(this.formGroup.value)
+  }
+
+  inputChange(serviceName: string): void {
+    this.selection.set(serviceName)
   }
 }

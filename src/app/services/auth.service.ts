7fa4b22f644b9f4b053a22b74070f617ca164520
Marina Rakovskaia <m.rakovskaia@gmail.com>
2024-12-03 15:46:44 +0100

    auth service created

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,63 @@
 import { Injectable, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  readonly isAuth = signal(false);
+  private apiUrl = 'http://localhost:8080/api/auth';
 
-  constructor(public router: Router) {}
+  constructor(private http: HttpClient) {}
 
-  public toogleAuth(): void {
-    this.isAuth.set(!this.isAuth());
-    if (!this.isAuth()) this.router.navigate(['']);
+  login(credentials: User): Observable<any> {
+    return this.http
+      .post<{ accessToken: string }>(`${this.apiUrl}/login`, credentials)
+      .pipe(
+        tap((response: { accessToken: string }) => {
+          localStorage.setItem('token', response.accessToken); // Store the token
+        })
+      );
   }
+
+  register(user: User): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, user); // Endpoint for registration
+  }
+  getUserRoles(): string[] {
+    // Get token from localStorage
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return [];
+    }
+
+    try {
+      // Decode the JWT token
+      const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+      // Return roles from token payload, or empty array if no roles found
+      return tokenPayload.roles || [];
+    } catch (error) {
+      console.error('Error decoding token:', error);
+      return [];
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem('token'); // Remove the token
+  }
+
+  isAuthenticated(): boolean {
+    // Logique pour vérifier si l'utilisateur est authentifié
+    return !!localStorage.getItem('token');
+  }
+
+  // readonly isAuth = signal(false);
+
+  // constructor(public router: Router) {}
+
+  // public toogleAuth(): void {
+  //   this.isAuth.set(!this.isAuth());
+  //   if (!this.isAuth()) this.router.navigate(['']);
+  // }
 }
